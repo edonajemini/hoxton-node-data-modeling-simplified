8dcd786b0390afe417376f1daa750a28df05bdef
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-09-11 14:59:21 +0200

    first commit

---
+++ db/setup.ts
@@ -0,0 +1,268 @@
+import Database from "better-sqlite3";
+const db = new Database('./db/data.db', { verbose: console.log });
+
+
+function createApplicants(){
+    const applicants = [
+        {
+            name: "Lara Hancock",
+            age:22,
+            position:"Receptionist"
+        },
+        {
+            name: "Bryant Higgins",
+            age:28,
+            position:"Assistant Director"
+        },
+        {
+            name: "Luna Johnson",
+            age:26,
+            position:"Office manager"
+        },
+        {
+            name: "Conrad Sharp",
+            age:31,
+            position:"Purchasing manager"
+        },
+        {
+            name: "Veronica Beck",
+            age:34,
+            position:"Operations manager"
+        }
+    ]
+
+const dropApplicantsTable = db.prepare(`
+    DROP TABLE IF EXISTS applicants;
+`)
+dropApplicantsTable.run();
+
+const createApplicantsTable = db.prepare(`
+    CREATE TABLE IF NOT EXISTS applicants (
+        id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        position TEXT NOT NULL,
+        PRIMARY KEY (id)
+    );
+`)
+createApplicantsTable.run();
+
+
+
+const createNewApplicant = db.prepare(`
+    INSERT INTO applicants (name, age, position) VALUES (?,?,?);
+`)
+
+for(let applicant of applicants){
+    createNewApplicant.run(applicant.name, applicant.age, applicant.position);
+}
+}
+
+function createInterviewers(){
+    
+    const interviewers = [
+        {
+            name: "Brantley Shelton",
+            companyId:1
+        },
+        {
+            name: "Warner Parris",
+            companyId:2
+        },
+        {
+            name: "Amy Daley",
+            companyId:4
+        },
+        {
+            name: "Frederick Garland",
+            companyId:3
+        }
+    ]
+
+    const dropInterviewersTable = db.prepare(`
+        DROP TABLE IF EXISTS interviewers;
+    `)
+    dropInterviewersTable.run();
+
+    const createInterviewersTable = db.prepare(`
+        CREATE TABLE IF NOT EXISTS interviewers (
+            id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            companyId INTEGER NOT NULL,
+            PRIMARY KEY (id)
+        );
+    `)
+    createInterviewersTable.run();
+
+    const createNewInterviewer = db.prepare(`
+        INSERT INTO interviewers (name, companyId) VALUES (?,?);
+    `)
+
+    for(let interviewer of interviewers){
+        createNewInterviewer.run(interviewer.name, interviewer.companyId);
+    }
+}
+
+function createInterviews(){
+    const interviews = [
+        {
+            applicantsId: 1,
+            interviewersId:2,
+            time:"12/10/2022",
+            place:""
+        },
+        {
+            applicantsId: 2,
+            interviewersId:3,
+            time:"12/10/2022",
+            place:""
+        },
+        {
+            applicantsId: 3,
+            interviewersId:1,
+            time:"12/10/2022",
+            place:""
+        },
+        {
+            applicantsId: 4,
+            interviewersId:2,
+            time:"12/10/2022",
+            place:""
+        }
+    ]
+    
+    const dropInterviewsTable = db.prepare(`
+        DROP TABLE IF EXISTS interviews;
+    `)
+    dropInterviewsTable.run();
+
+    const createInterviewsTable = db.prepare(`
+            CREATE TABLE IF NOT EXISTS interviews (
+                id INTEGER NOT NULL,
+                applicantId INTEGER NOT NULL,
+                interviewerId INTEGER  NOT NULL,
+                time TEXT,
+                place TEXT,
+                PRIMARY KEY (id),
+                FOREIGN KEY (applicantId) REFERENCES applicants(id) ON DELETE CASCADE,
+                FOREIGN KEY (interviewerId) REFERENCES interviewers(id) ON DELETE CASCADE
+            );
+        `)
+        createInterviewsTable.run();
+
+    
+        const createNewInterview = db.prepare(`
+            INSERT INTO interviews (applicantId, interviewerId, time, place) VALUES (?,?,?,?);
+        `)
+    
+        for(let interview of interviews){
+            createNewInterview.run(interview.applicantsId, interview.interviewersId, interview.time, interview.place);
+        }
+}
+
+function createCompanies(){
+        
+    const companies = [
+        {
+            name:"Google",
+            email:"google@google.com"
+        },
+        {
+            name:"Apple",
+            email:"apple@apple.com"
+        },
+        {
+            name:"Amazon",
+            email:"amazon@amazon.com"
+        },
+        {
+            name:"Walmart",
+            email:"walmart@walmart.com"
+        },
+    ]
+    
+    const dropCompaniesTable = db.prepare(`
+        DROP TABLE IF EXISTS companies;
+    `)
+    dropCompaniesTable.run();
+    
+    const createCompaniesTable = db.prepare(`
+        CREATE TABLE IF NOT EXISTS companies (
+            id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            PRIMARY KEY (id)
+        );
+    `)
+    createCompaniesTable.run();
+    
+    const createNewCompany = db.prepare(`
+        INSERT INTO companies (name, email) VALUES (?, ?);
+    `)
+    
+    for(let company of companies){
+        createNewCompany.run(company.name, company.email);
+    }
+}
+
+function createEmployees(){
+            
+     const employees = [
+        {
+             name: "Brantley Shelton",
+             email: "BrantleyShelton@google.com",
+             position: "Product Manager",
+             companyId: 1
+        },
+        {
+            name: "Warner Parris",
+            email: "WarnerParris@gmail.com",
+            position: "Team Leader",
+            companyId: 2
+        },
+        {
+            name: "Amy Daley",
+            email: "Amy Daley@apple.com",
+            position: "Executive",
+            companyId: 3
+        },
+        {
+            name: "Frederick Garland",
+            email:"FrederickGarland@gmail.com",
+            position:"Assistant Manager",
+            companyId:4
+        }
+        ]
+
+        const dropEmployeesTable = db.prepare(`
+            DROP TABLE IF EXISTS employees;
+        `)
+        dropEmployeesTable.run();
+
+        const createEmployeesTable = db.prepare(`
+            CREATE TABLE IF NOT EXISTS employees (
+                id INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                position TEXT NOT NULL,
+                companyId INTEGER NOT NULL,
+                PRIMARY KEY (id),
+                FOREIGN KEY (companyId) REFERENCES companies(id) ON DELETE CASCADE
+            );
+        `)
+        createEmployeesTable.run();
+
+        const createNewEmployee = db.prepare(`
+            INSERT INTO employees (name, email, position, companyId) VALUES (?,?,?,?);
+        `)
+
+        for(let employee of employees){
+            createNewEmployee.run(employee.name, employee.email, employee.position, employee.companyId);
+        }
+}
+
+createCompanies();
+createEmployees();
+createApplicants();
+createInterviewers();
+createInterviews();
---
+++ index.ts
@@ -0,0 +1,208 @@
+import express from "express"
+import cors from "cors"
+import Database from 'better-sqlite3'
+const db = Database('./db/data.db', { verbose: console.log })
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send(`
+    <ul>
+    <li><a href="/applicants">Applicants</a></li>
+    <li><a href="/interviewers">Interviewers</a></li>
+    <li><a href="/interviews">Interviews</a></li>
+    </ul>
+    `)
+  })
+  
+ 
+  const getApplicants = db.prepare(`
+  SELECT * FROM applicants;
+  `)
+  const getInterviewers = db.prepare(`
+  SELECT * FROM interviewers;
+  `)
+  const getInterviews = db.prepare(`
+  SELECT * FROM interviewers;
+  `)
+  
+  const getApplicantsById = db.prepare(`
+  SELECT * FROM applicants WHERE id = ?;
+  `)
+  const getInterviewersById = db.prepare(`
+SELECT * FROM interviewers WHERE id = ?;
+`)
+const getInterviewsById = db.prepare(`
+SELECT * FROM interviews WHERE id = ?;
+`)
+  const getInterviewsForApplicants = db.prepare(`
+  SELECT * FROM interviews WHERE applicantsId = ?;
+  `)
+  const getInterviewsForInterviewers = db.prepare(`
+  SELECT * FROM interviews WHERE interviewersId = ?;
+  `)
+  const getInterviewersForApplicants = db.prepare(`
+  SELECT interviewers.* FROM interviewers
+  JOIN interviews ON interviewers.id = interviews.interviewersId
+  WHERE interviews.applicantsId = ?;
+  `)
+  const getApplicantsForInterviewers = db.prepare(`
+SELECT applicants.* FROM applicants
+JOIN interviews ON applicants.id = interviews.applicantsId
+WHERE interviews.interviewersId = ?;
+`)
+  
+
+//get applicants with their interviews and interviewers
+  app.get('/applicants', (req, res) => {
+    const applicants = getApplicants.all()
+    for(let applicant of applicants){
+        applicant.interviews = getInterviewsForApplicants.all(applicant.id)
+        applicant.interviewers = getInterviewersForApplicants.all(applicant.id)
+    }
+    res.send(applicants)
+  })
+
+  //get applicants by id
+  app.get('/applicants/:id', (req, res) => {
+    const id = Number(req.params.id)
+  const applicant = getApplicantsById.get(id)
+    if (applicant) {
+        applicant.interviews = getInterviewsForApplicants.all(applicant.id)
+        applicant.interviewers = getInterviewersForApplicants.all(applicant.id)
+      res.send(applicant)
+    } else {
+      res.status(404).send({ error: 'Applicants not found' })
+    }
+  })
+
+  //get Interviewers with their interviews and applicants
+  app.get('/interviewers', (req, res) => {
+      const interviewers = getInterviewers.all()
+      for(let interviewer of interviewers){
+        interviewer.interviews = getInterviewsForInterviewers.all(interviewer.id)
+        interviewer.applicants = getApplicantsForInterviewers.all(interviewer.id)
+    }
+      res.send(interviewers)
+  })
+
+  //get Interviewers by Id
+app.get('/interviewers/:id', (req, res) => {
+    const id = Number(req.params.id)
+  const interviewer = getInterviewersById.get(id)
+    if (interviewer) {
+        interviewer.interviews = getInterviewsForInterviewers.all(interviewer.id)
+        interviewer.applicants = getApplicantsForInterviewers.all(interviewer.id)
+      res.send(interviewer)
+    } else {
+      res.status(404).send({ error: 'Interviewer not found' })
+    }
+  })
+
+  //get Interviews
+  app.get('/interviews', (req, res) => {
+    const interviews = getInterviews.all()
+    res.send(interviews)
+  })
+
+
+  // Post applicants
+  const postApplicant = db.prepare(`
+  INSERT INTO applicants (name, age, position) VALUES (?, ?, ?);
+  `)
+
+  app.post('/applicants', (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    const position = req.body.position
+      let errors: string[] = []
+      
+      if (typeof req.body.name !== 'string') {
+          errors.push('Add a proper NAME!')
+        }
+     
+      if(typeof req.body.age  !=='number') {
+          errors.push('Add a proper TYPE OF AGE')
+      }
+      if (typeof req.body.position !== 'string') {
+        errors.push('Add a proper POSITION!')
+      }
+      if( errors.length === 0)  {
+        const applicantInfo = postApplicant.run(name, age, position)
+        const newApplicants = getApplicantsById.get(applicantInfo.lastInsertRowid)
+        res.send(newApplicants)
+      }
+      else {
+          res.status(400).send({ errors: errors })
+        }
+  })
+   // Post Interviewer
+   const postInterviewer = db.prepare(`
+  INSERT INTO interviewers (name, company) VALUES (?, ?);
+  `)
+
+  app.post('/interviewers', (req, res) => {
+    const name = req.body.name
+    const company = req.body.company
+      let errors: string[] = []
+      
+      if (typeof req.body.name !== 'string') {
+          errors.push('Add a proper NAME!')
+        }
+     
+      if(typeof req.body.company  !=='string') {
+          errors.push('Add a proper TYPE OF company')
+      }
+      if( errors.length === 0)  {
+        const interviewerInfo = postInterviewer.run(name, company)
+        const newInterviewer = getInterviewersById.get(interviewerInfo.lastInsertRowid)
+        res.send(newInterviewer)
+      }
+      else {
+          res.status(400).send({ errors: errors })
+        }
+  })
+    // Post Interviews
+    const postInterviews = db.prepare(`
+    INSERT INTO interviews (applicantsId, interviewersId, time, place) VALUES (?, ?, ?, ?);
+    `)
+  
+    app.post('/interviews', (req, res) => {
+      const applicantsId = req.body.applicantsId
+      const interviewersId = req.body.interviewersId
+      const time = req.body.time
+      const place = req.body.place
+        let errors: string[] = []
+        
+        if (typeof req.body.applicantsId !== 'number') {
+            errors.push('Add a proper applicant Id!')
+          }
+        if(typeof req.body.interviewersId  !=='number') {
+            errors.push('Add a proper interviewer Id')
+        }
+        if(typeof req.body.time  !=='string') {
+            errors.push('Add a proper TIME')
+        }
+        if(typeof req.body.place  !=='string') {
+            errors.push('Add a proper place')
+        }
+        if( errors.length === 0)  {
+          const interviewInfo = postInterviews.run(applicantsId, interviewersId, time, place)
+          const newInterviews = getInterviewsById.get(interviewInfo.lastInsertRowid)
+          res.send(newInterviews)
+        }
+        else {
+            res.status(400).send({ errors: errors })
+          }
+    })
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
+  
+
